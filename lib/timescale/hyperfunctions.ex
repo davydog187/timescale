4f37559428038f8e808b6f47d759c87ba557c2dc
Sean Callan <sean.callan@stord.com>
2022-08-20 10:26:31 -0400

    Add `first/2` to hyperfunctions

--- lib/timescale/hyperfunctions.ex
+++ lib/timescale/hyperfunctions.ex
@@ -1,4 +1,24 @@
 defmodule Timescale.Hyperfunctions do
+  @moduledoc """
+  Timescale hyperfunctions are a specialized set of functions that allow you to analyze time-series data. 
+  You can use hyperfunctions to analyze anything you have stored as time-series data, including IoT devices, 
+  IT systems, marketing analytics, user behavior, financial metrics, and cryptocurrency.
+
+  [Documentation](https://docs.timescale.com/api/latest/hyperfunctions/)
+  """
+
+  @doc """
+  The first aggregate allows you to get the value of one column as ordered by another. 
+  For example, `first(temperature, time)` returns the earliest temperature value based on time within an aggregate group.
+
+  [Documentation](https://docs.timescale.com/api/latest/hyperfunctions/first/)
+  """
+  defmacro first(field, time) do
+    quote do
+      fragment("first(?, ?)", unquote(field), unquote(time))
+    end
+  end
+
   @doc """
   The last aggregate allows you to get the value of one column as ordered by another.
   For example, `last(temperature, time)` returns the latest temperature value based on time within an aggregate group.
--- test/timescale/hyperfunction_test.exs
+++ test/timescale/hyperfunction_test.exs
@@ -4,7 +4,14 @@ defmodule Timescale.HyperfunctionTest do
   alias TimescaleApp.Table
 
   import Timescale.Hyperfunctions
-  import Ecto.Query, warn: false, except: [last: 2]
+  import Ecto.Query, warn: false, except: [first: 2, last: 2]
+
+  test "first/2 generates a valid query" do
+    assert_sql(
+      from(r in Table, select: first(r.a, r.timestamp)),
+      ~s[SELECT first(m0."a", m0."timestamp") FROM "my_table" AS m0]
+    )
+  end
 
   test "last/2 generates a valid query" do
     assert_sql(
