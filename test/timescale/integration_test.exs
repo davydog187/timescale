31f1cf80f625b0003a74a8b160a6a3671846aa18
Dave Lucia <davelucianyc@gmail.com>
2022-08-27 09:40:35 -0400

    Add an integration test for histogram/4

--- test/timescale/integration_test.exs
+++ test/timescale/integration_test.exs
@@ -7,20 +7,34 @@ defmodule Timescale.IntegrationTest do
   alias TimescaleApp.Repo
   alias TimescaleApp.Table
 
-  setup do
+  test "first/2 returns the first value" do
     fixture(1.0)
     fixture(2.0)
     fixture(3.0)
 
-    :ok
-  end
-
-  test "first/2 returns the first value" do
     assert Repo.one(from(t in Table, select: first(t.field, t.timestamp))) == 1.0
   end
 
   test "last/2 returns the last value" do
+    fixture(1.0)
+    fixture(2.0)
+    fixture(3.0)
+
     assert Repo.one(from(t in Table, select: last(t.field, t.timestamp))) == 3.0
+  end
+
+  test "histogram/4 returns a histogram" do
+    for x <- 1..100, do: fixture(x / 1.0)
+
+    assert Repo.one(from(t in Table, select: histogram(t.field, 0, 100.0, 5))) == [
+             0,
+             19,
+             20,
+             20,
+             20,
+             20,
+             1
+           ]
   end
 
   def fixture(value, timestamp \\ NaiveDateTime.utc_now()) do
