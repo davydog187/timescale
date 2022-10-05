003e5937b23999d71796f6244b62e1c92b301ac7
Dave Lucia <davelucianyc@gmail.com>
2022-10-05 15:38:19 -0400

    Introduce remove_compression_policy

--- priv/repo/migrations/20220826223241_hypertable.exs
+++ priv/repo/migrations/20220826223241_hypertable.exs
@@ -12,5 +12,6 @@ defmodule TimescaleApp.Repo.Migrations.Hypertable do
     create_hypertable(:test_hypertable, :timestamp)
     enable_hypertable_compression(:test_hypertable, segment_by: :timestamp)
     add_compression_policy(:test_hypertable, "60d")
+    remove_compression_policy(:test_hypertable, if_exists: true)
   end
 end
--- test/timescale/migration_test.exs
+++ test/timescale/migration_test.exs
@@ -14,4 +14,17 @@ defmodule Timescale.MigrationTest do
                    """)
                  end
   end
+
+  test "remove_compression_policy/3 should raise an error if an invalid option is provided" do
+    assert_raise Timescale.MigrationArgError,
+                 "The remove_compression_policy TimescaleDB function does support the following options: [:if_not_exists]",
+                 fn ->
+                   Code.eval_string("""
+                   import Ecto.Query
+                   import Timescale.Migration
+
+                   remove_compression_policy(:my_ts_table, if_not_exists: true)
+                   """)
+                 end
+  end
 end
